2bd84c1728a46300222a3ff897334a8f24350124
J Z <ubuntu@jzhome>
2019-02-24 01:09:22 -0800

    json file for parameters

--- src/cmf/comb_motif.java
+++ src/cmf/comb_motif.java
@@ -3,7 +3,19 @@ package cmf;
 /**
  *
  * @comb_motif.pl
+ * TO DO , only used in cmfinder.pl
  */
 public class comb_motif {
-    
+
+    //debug use
+    static {
+        System.out.println("comb_motif class");
+    }
+
+    int MAX_GAP = 100;
+    double len_energy_threshold = 0.1;
+    int min_overlap = 2;
+    double min_num = 2.5;
+    int max_len = 200;
+
 }
