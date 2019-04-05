6e3bd3b01bbf3a8d3373a713c00e861de005dc7a
Joyce Zhou <chymeraclaw@gmail.com>
2019-04-04 17:13:03 -0700

    change MAFreader to an iterator

--- src/blockmerge/util/MAFReader.java
+++ src/blockmerge/util/MAFReader.java
@@ -8,17 +8,30 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class MAFReader {
+public class MAFReader implements Iterator<AlignmentBlock> {
     private String filepath;
+    private BufferedReader r;
+    private Iterator<String> it;
 
-    public MAFReader(String filepath) {
+    public MAFReader(String filepath) throws IOException {
         this.filepath = filepath;
+        this.r = Files.newBufferedReader(Paths.get("data", filepath));
+        this.it = this.r.lines().iterator();
     }
 
-    public AlignmentBlock getNextBlock() throws IOException {
+
+    public void reset() {
+        this.it = r.lines().iterator();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return this.it.hasNext();
+    }
+
+    @Override
+    public AlignmentBlock next() {
         List<String> lines = new ArrayList<>();
-        BufferedReader r = Files.newBufferedReader(Paths.get("data", filepath));
-        Iterator<String> it = r.lines().iterator();
         while (it.hasNext()) {
             String s = it.next();
             if (!s.isEmpty()) {
@@ -31,5 +44,4 @@ public class MAFReader {
         System.out.print("done");
         return result;
     }
-
 }
