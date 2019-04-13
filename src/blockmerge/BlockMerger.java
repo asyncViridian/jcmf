5d891c2c79f9f19b6233a67f6bfd08ddbd466b51
Joyce Zhou <chymeraclaw@gmail.com>
2019-04-13 14:33:53 -0700

    start writing test files for merge

--- src/blockmerge/BlockMerger.java
+++ src/blockmerge/BlockMerger.java
@@ -4,7 +4,6 @@ import blockmerge.util.AlignmentBlock;
 import blockmerge.util.MAFReader;
 
 import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.file.Files;
