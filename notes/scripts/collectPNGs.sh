3d52969559dc926054081a4521ad3ac448152661
Joyce Zhou <chymeraclaw@gmail.com>
2019-06-25 23:52:03 -0700

    fix bug in collectpngs

--- notes/scripts/collectPNGs.sh
+++ notes/scripts/collectPNGs.sh
@@ -1,6 +1,10 @@
 # arg1 = directory to get all images from
 # arg2 = directory to put PNGs in
-find $1 -regex '.*\(jpg\|jpeg\|png\|gif\)' \! -path './$2/*' -exec echo cp -t $2 {} + > copyphoto.sh
-chmod +x copyphoto.sh
-./copyphoto.sh
-rm copyphoto.sh
+
+# get an id (the same as the pid of this script) to match the copyphoto script this generates
+id=$$
+
+find $1 -regex '.*\(jpg\|jpeg\|png\|gif\)' \! -path './$2/*' -exec echo cp -t $2 {} + > copyphoto${id}.sh
+chmod +x copyphoto${id}.sh
+./copyphoto${id}.sh
+rm copyphoto${id}.sh
